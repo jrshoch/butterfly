aeff3b5297612b874fe8953260d2e4a5dab239c2
Jason Hoch <jrshoch@gmail.com>
2012-10-13 01:04:32 -0400

    Adds prefs and sets RobotPlayer to proper format

--- teams/team000/RobotPlayer.java
+++ teams/team000/RobotPlayer.java
@@ -4,9 +4,9 @@ import battlecode.common.RobotController;
 
 public class RobotPlayer {
 
-  public static void run(RobotController myRC) {
-    while (true) {
-      myRC.yield();
+    public static void run(RobotController myRC) {
+        while (true) {
+            myRC.yield();
+        }
     }
-  }
 }
