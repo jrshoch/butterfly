83a17f75c8a41e3116b61e76e712195a5dd31ae9
Jason Hoch <jrshoch@gmail.com>
2012-10-13 00:45:57 -0400

    Adds our team's RobotPlayer

---
+++ teams/team000/RobotPlayer.java
@@ -0,0 +1,12 @@
+package team000;
+
+import battlecode.common.RobotController;
+
+public class RobotPlayer {
+
+  public static void run(RobotController myRC) {
+    while (true) {
+      myRC.yield();
+    }
+  }
+}
